6a1f083e0073883b2210e3908e0f6cc30eeb0d3d
Issa <abdu2997@edu.easj.dk>
2018-11-30 16:13:29 +0100

    UdpSendEnviroment Data

--- BerthaFakeDataUdpSender/Program.cs
+++ BerthaFakeDataUdpSender/Program.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BerthaFakeDataUdpSender
@@ -10,12 +13,52 @@ namespace BerthaFakeDataUdpSender
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int number = 1;
+            int UserId = 93;
+            decimal Humidity = 3;
+            decimal Temperature = 5;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+            string Location = "Cph";
+
+
+            //Random rnCo = new Random();
+            //Random rnNox = new Random();
+
+            //string sensorLocation = "Pollution sensor v.1.0. \n" + "Location: Jernbanegade 3 1\n";
+
+            UdpClient udpServer = new UdpClient(0);
+            udpServer.EnableBroadcast = true;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 9000);
+
+            Console.WriteLine("Broadcast ready. Get started Press Enter");
+            Console.ReadLine();
+
+            while (true)
+            {
+
+
+                DateTime currentTime = DateTime.Now;
+                string timeTxt = "Time: " + currentTime + "\n";
+                string data = "UserId " + UserId + "\n" + "Humidity " + Humidity + "\n" + "Temperature " + Temperature + "\n" + "Location " + Location + "\n";
+                string sensorData = timeTxt + data;
+
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(sensorData);
+
+                try
+                {
+                    udpServer.Send(sendBytes, sendBytes.Length, endPoint); //, endPoint
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                Console.WriteLine("New Data has been sent " + number++);
+                Thread.Sleep(100000);
+            }
+
         }
     }
+    
 }
